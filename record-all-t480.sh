dcef9b133a3587dc10936180e9d40fd5bd351e68
matt venn <matt@mattvenn.net>
2020-09-08 14:45:12 +0200

    tested mp4 output to premier

--- record-all-t480.sh
+++ record-all-t480.sh
@@ -8,17 +8,15 @@ v4l2-ctl -d $webcamdevice --set-ctrl=focus_auto=0           # no auto focus
 v4l2-ctl -d $webcamdevice -L
 
 # vcodec h264 was important for webcam
-# -c:v libx264rgb -crf 0 and -preset ultrafast says don't try to make small file sizes
+# -c:v libx264rgb doesn't import into premier, changed to libx264
+# -crf 0 and -preset ultrafast says don't try to make small file sizes, changed to -crf 20 to try smaller files but haven't done burn test
 # removed the half sizing of the screen
+# changed extension to mp4 to avoid copying from mkv
 
 ffmpeg -loglevel error -stats \
     -f alsa -ac 2 -sample_fmt s16 -thread_queue_size $thread_queue_size -i hw:1,0 -async 1 \
     -f v4l2 -video_size $webcamsize -framerate $framerate -vcodec h264 -thread_queue_size $thread_queue_size -i $webcamdevice\
     -f x11grab -video_size $screensize -framerate $framerate -thread_queue_size $thread_queue_size -i :0.0+0,0 \
     -map 0:a:0 $1.wav \
-    -map 1:v:0 -c:v libx264 -crf 20 -preset ultrafast $1-webcam.mkv \
-    -map 2:v:0 -c:v libx264 -crf 20 -preset ultrafast $1-screen.mkv \
-
-# afterwards
-# ffmpeg -i interview-screen.mkv -strict -2 -f mp4 interview-screen.mp4
-# works for making them openable in premier
+    -map 1:v:0 -c:v libx264 -crf 20 -preset ultrafast $1-webcam.mp4 \
+    -map 2:v:0 -c:v libx264 -crf 20 -preset ultrafast $1-screen.mp4 \
