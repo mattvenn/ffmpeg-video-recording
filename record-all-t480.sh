196dc0205e75c25240e75f92b4200f0bb721eed4
matt venn <matt@mattvenn.net>
2020-09-28 15:05:50 +0200

    bigger thread size, add probesize to x11, remove async from audio

--- record-all-t480.sh
+++ record-all-t480.sh
@@ -1,14 +1,14 @@
 framerate=30
 screensize=2560x1600
 webcamsize=1280x800
-thread_queue_size=1024
+thread_queue_size=8096
 compression=0
 webcamdevice=/dev/video2 # either 0 or 2 depending on usb
 
 # setup the webcam to 50hz and no autofocus
 v4l2-ctl -d $webcamdevice --set-ctrl=power_line_frequency=1 # 50 hz
 v4l2-ctl -d $webcamdevice --set-ctrl=focus_auto=0           # no auto focus
-v4l2-ctl -d $webcamdevice -L
+#v4l2-ctl -d $webcamdevice -L
 
 # vcodec h264 was important for webcam
 # -c:v libx264rgb doesn't import into premier, changed to libx264
@@ -16,10 +16,10 @@ v4l2-ctl -d $webcamdevice -L
 # removed the half sizing of the screen
 # changed extension to mp4 to avoid copying from mkv
 
-ffmpeg -loglevel error -stats \
-    -f alsa -ac 2 -sample_fmt s16 -thread_queue_size $thread_queue_size -i hw:1,0 -async 1 \
+ffmpeg -loglevel warning -stats \
+    -f alsa -ac 2 -sample_fmt s16 -thread_queue_size $thread_queue_size -i hw:1,0 \
     -f v4l2 -video_size $webcamsize -framerate $framerate -vcodec h264 -thread_queue_size $thread_queue_size -i $webcamdevice\
-    -f x11grab -video_size $screensize -framerate $framerate -thread_queue_size $thread_queue_size -i :0.0+0,0 \
+    -f x11grab -video_size $screensize -framerate $framerate -probesize 40M -thread_queue_size $thread_queue_size -i :0.0+0,0 \
     -map 0:a:0 $1.wav \
     -map 1:v:0 -c:v libx264 -crf $compression -preset ultrafast $1-webcam.mp4 \
     -map 2:v:0 -c:v libx264 -crf $compression -preset ultrafast $1-screen.mp4 \
